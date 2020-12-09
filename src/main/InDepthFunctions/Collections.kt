45a03c12ec5ccc38a195751c1293c4213e2670ce
abehes <abehesn@calstatela.edu>
2020-12-08 21:22:08 -0800

    A demonstration of an implementation of various collections as well as easier function calls within Kotlin.

---
+++ src/main/InDepthFunctions/Collections.kt
@@ -0,0 +1,44 @@
+package main.InDepthFunctions
+
+// This file serves as a demonstration for various [Collection] implemenetations
+
+/**
+ * An implementation of [HashSet] in Kotlin
+ */
+val set = hashSetOf(1, 2, 3)
+
+/**
+ * An implementation of [List] in Kotlin
+ */
+val list = listOf(4, 5, 6)
+
+/**
+ * An implementation of [Map] in Kotlin
+ */
+val map = hashMapOf(1 to "one", 2 to "two", 3 to "three")
+
+/**
+ * An imeplementation of [ArrayList] in Kotlin
+ */
+val arr = arrayListOf("sixty", "eighty", "ninety")
+
+/**
+ * Finds the last element within an array
+ */
+fun lastElement(arr: ArrayList<String>) = arr.last()
+
+/**
+ * Finds the first element within an array
+ */
+fun firstElement(arr: ArrayList<String>) = arr.first()
+
+/**
+ * Finds the biggest number within a list
+ */
+fun biggestNumber(list: List<Int>) = list.max()
+
+/**
+ * Finds the smallest number within a list
+ */
+fun smallestNumber(list: List<Int>) = list.min()
+
---
+++ src/main/InDepthFunctions/EasierFunctionCalls.kt
@@ -0,0 +1,44 @@
+package main.InDepthFunctions
+
+// This file demonstrates how easy function calls within Kotlin are
+
+fun main() {
+    // Create an empty int array
+    val anArr : ArrayList<Int> = arrayListOf()
+
+    // Add numbers 1 to 10 to array
+    for (i in 1..10)
+        anArr.add(i)
+
+    // Create a joint string of the collection
+    val jointString = joinToString(anArr, ", ", "(", ")")
+
+    // Print its value
+    println(jointString)
+}
+
+/**
+ * A demonstration of how printing the list automatically the .toString method
+ */
+fun printList() {
+    println(list)
+}
+
+/**
+ * A function that can be called on an array to join the elements as a string
+ * in a special way
+ */
+fun <T> joinToString(
+    collection: Collection<T>,
+    separator: String,
+    prefix: String,
+    postfix: String
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
