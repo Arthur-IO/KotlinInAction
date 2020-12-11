4a87c915856587b9a85b71c16432d500292a5804
abehes <abehesn@calstatela.edu>
2020-12-10 19:24:27 -0800

    A demonstration of accessing backing fields for setter properties, data classes and class delegation, implementing properties and the 'object' keyword

---
+++ src/main/KotlinInAction/Part_1_4_Classes_Objects_And_Interfaces/AccessingBackingField.kt
@@ -0,0 +1,59 @@
+package main.KotlinInAction.Part_1_4_Classes_Objects_And_Interfaces
+
+// This file demonstrates properties accessing backing field in a 'setter' and 'getter
+
+fun main(args: Array<String>) {
+    val GOOG = Stock(345)
+    while(true) {
+        Thread.sleep(1000L)
+        GOOG.updateStock()
+    }
+}
+
+/**
+ * A simple class that represents a person
+ */
+class Person(val name: String) {
+    /**
+     * A property which holds the address of a [Person]
+     *
+     * This field has a default value and a 'setter' that can
+     * access it's backing field
+     */
+    var address: String = "undefined"
+        set(value) {
+            println("$name's address was modified.")
+            field = value
+        }
+}
+
+/**
+ * A stock object that takes an initial stock value which
+ * also appreciates in value as time passes
+ */
+class Stock(var initialValue: Int) {
+
+    /**
+     * The stocks initial age
+     */
+    val stockAge = System.currentTimeMillis()
+
+    /**
+     * The stocks initial value
+     */
+    var stockValue: Int = initialValue
+        private set(value) {
+            println("Stock grew in value by $${value - stockValue}")
+            field = value
+        }
+
+    /**
+     * Updates the stock value based on the time that has passed
+     */
+    fun updateStock() {
+        val timePassed = System.currentTimeMillis() - stockAge
+        stockValue *= (timePassed / 1000).toInt()
+    }
+
+}
+
--- src/main/KotlinInAction/Part_1_4_Classes_Objects_And_Interfaces/ImplementingProperties.kt
+++ src/main/KotlinInAction/Part_1_4_Classes_Objects_And_Interfaces/ImplementingProperties.kt
@@ -1,2 +1,36 @@
 package main.KotlinInAction.Part_1_4_Classes_Objects_And_Interfaces
 
+// This file demonstrates interfaces with abstract properties
+
+/**
+ * An interface with an abstract property
+ */
+interface Component {
+    val id: String
+}
+
+/**
+ * An implementation of [Component] with initialized property
+ */
+class Ram(val partNumber: Int): Component {
+    override val id: String
+        get() = "#$partNumber"
+}
+
+/**
+ * An implementation of [Component] with initialized property
+ */
+class Case(val caseName: String): Component {
+    override val id: String
+        get() = "__$caseName"
+}
+
+/**
+ * An implementation of [Component] with initialized property
+ */
+class CPU(val cpuName: String, val brand: String): Component {
+    override val id: String = createId()
+
+    fun createId() = "${brand}_$cpuName"
+
+}
